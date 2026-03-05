28d4cf95ff1ac887dd800f02291c5b22cfed0c19
srinidhi-2005 <marthasrinidhi16@gmail.com>
2026-03-05 14:45:11 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# training data
+X = np.array([[1],[2],[3],[4],[5]])
+y = np.array([2,4,6,8,10])
+
+# train model
+model = LinearRegression()
+model.fit(X,y)
+
+# prediction
+value = np.array([[6]])
+prediction = model.predict(value)
+
+print("Prediction:", prediction[0])
